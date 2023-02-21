a192f1bd3472fbb4f3ea7388fa36ba4c697d9e06
valerii <valerii.rivne@gmail.com>
2023-02-21 23:52:44 +0200

    some little changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import ProductList from './components/ProductList';
+import styled from 'styled-components';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
+    <AppContainer>
       <h1>My E-Shop</h1>
       <ProductList />
-    </div>
+    </AppContainer>
   );
-}
+};
+const AppContainer = styled.div`
+  text-align: center;
+  color: #3e8e41;
+`;
 
 export default App;
